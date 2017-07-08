7ab233dc1ef52ec2197d77640e5a66406ad3da9c
Yoshua Nahar <yosh.nahar@gmail.com>
2017-07-08 12:56:29 +0200

    add missed file

--- todo_app-frontend/src/app/shared/TodosHttpService.ts
+++ todo_app-frontend/src/app/shared/TodosHttpService.ts
@@ -5,7 +5,7 @@ import { Todo } from "./Todo";
 @Injectable()
 export class TodosHttpService {
 
-  private static URL = `http://130.211.68.181:8080${window.location.pathname}`;
+  private static DOMAIN = "http://130.211.68.181:8080/"; // http://localhost:8080/
   private static OPTIONS = new RequestOptions(new Headers({ "Content-Type": "application/json" }));
 
   private static handleError(error: any): Promise<any> {
@@ -13,36 +13,43 @@ export class TodosHttpService {
     return Promise.reject(error.message || error);
   }
 
+  private static getFullUrl(): string {
+    const url = window.location.pathname;
+    const i = url.lastIndexOf("/");
+
+    return TodosHttpService.DOMAIN + url.substring(i + 1);
+  }
+
   constructor(private http: Http) {
   }
 
   getTodosFromServer(): Promise<Todo[]> {
-    console.log(TodosHttpService.URL); // testing
+    console.log(TodosHttpService.DOMAIN); // testing
 
     // https://stackoverflow.com/a/32495055/5473627
-    return this.http.get(TodosHttpService.URL)
+    return this.http.get(TodosHttpService.getFullUrl())
       .toPromise()
-      .then(response => response.json().todoList as Todo[])
+      .then(response => response.json().todos as Todo[])
       .catch(TodosHttpService.handleError);
     // the server sends { "url": "xxx", "todoList": [array with all the todos] }
   }
 
-  addTodoOnServer(todo: Todo): void {
-    this.http.put(TodosHttpService.URL, todo, TodosHttpService.OPTIONS)
+  putTodoOnServer(todo: Todo): void {
+    this.http.put(TodosHttpService.getFullUrl(), todo, TodosHttpService.OPTIONS)
       .toPromise()
       .catch(TodosHttpService.handleError);
     console.log(`put ${todo}`);
   }
 
   updateTodoOnServer(todo: Todo): void {
-    this.http.post(TodosHttpService.URL, todo, TodosHttpService.OPTIONS)
+    this.http.post(TodosHttpService.getFullUrl(), todo, TodosHttpService.OPTIONS)
       .toPromise()
       .catch(TodosHttpService.handleError);
     console.log(`update ${todo}`);
   }
 
   deleteTodoOnServer(id: number): void {
-    this.http.delete(`${TodosHttpService.URL}/${id}`, TodosHttpService.OPTIONS)
+    this.http.delete(`${TodosHttpService.getFullUrl()}/${id}`, TodosHttpService.OPTIONS)
       .toPromise()
       .catch(TodosHttpService.handleError);
     console.log(`delete ${id}`);
